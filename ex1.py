585e20ada6b14aa1412afca458a3e35a6faa40c5
belloau <alhamdubello@gmail.com>
2019-11-20 11:32:21 +0000

    lpthw first commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# Adding comments to your program is good
+# anything after the # is ignored in python
+
+print("Hello World")
+print("Hello Agan")
+print("I like typing this print statements")
+print("Really, learning to print out words")
+print("Yay! Printing is fun")
