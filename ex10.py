db45a3d789416794337a1c22fc2ee4662e8e60b2
belloau <alhamdubello@gmail.com>
2019-11-21 17:08:16 +0000

    compeleted exercise10

---
+++ ex10.py
@@ -0,0 +1,26 @@
+#More printing
+#
+#
+#
+#
+#
+#"I am 6'2\" tall" #escape double-quotes inside string
+#'I am 6\'2" tall.' #escape single-quotes
+
+
+tabby_cat = "\tI'm tabbed in."
+
+persian_cat = "I'm split\non a line"
+
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(fat_cat)
