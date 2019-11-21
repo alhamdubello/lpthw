bcfee1a9db6e76b8600d15006a3ae0b2680f66fc
belloau <alhamdubello@gmail.com>
2019-11-21 14:36:08 +0000

    sucessfully completed exercise 5

---
+++ ex5.py
@@ -0,0 +1,23 @@
+#working with variables and printing
+
+my_name = 'Mike Tyson'
+my_age  = 35 #not a lie
+my_height = 74 #inches
+my_weight = 180 # lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usulally {my_teeth} depending on the coffee.")
+
+
+#this line ois kinda tricky
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
