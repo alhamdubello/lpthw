4b84e64a7ef4737dd6bad424c7c8c145697f3aee
belloau <alhamdubello@gmail.com>
2019-11-21 16:52:37 +0000

    completed execrise9

---
+++ ex9.py
@@ -0,0 +1,17 @@
+#Printing Printing Printing
+#more printing on excersie 9
+#
+#
+#
+days = "Mon Tue Wed Thu Fri Sat Sun" #variable for days of the week
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #variable with new line
+
+print("Here are the days: ", days)
+print("Here are the months: ", months) #prints out new line
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""") # This is a multiline print comment
