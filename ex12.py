332396c9f8f051875f27d779b287ec69797ea26e
belloau <alhamdubello@gmail.com>
2019-11-22 11:25:21 +0000

    compeleted ex10 11 12

---
+++ ex12.py
@@ -0,0 +1,15 @@
+#more on prompting people
+#
+#
+y = input("Name? ") # getting input from user within a single input function
+
+print(f"My name is {y}") #printing the assigned variable
+
+
+age = input("How old are you? ")
+
+height = input("How tall are you?")
+
+weight = input("How heavy are you?")
+
+print(f"So, you're {age} old, {height} and weight {weight} heavy.")
