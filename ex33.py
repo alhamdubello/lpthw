20c4beda62dba7b34afdeb84bd9338d1c7c09247
belloau <alhamdubello@gmail.com>
2019-11-25 12:32:19 +0000

    completed ex38

---
+++ ex33.py
@@ -0,0 +1,20 @@
+#while-loops
+
+i = 0
+
+numbers = []
+
+
+while i < 20:
+    print(f"Testing printing {i}")
+    numbers.append(i)
+
+    i += 1
+
+    print("Numbers now", numbers)
+
+    print(f"At the bottom i is {i}.")
+print("The numbers: ")
+
+for num in numbers:
+    print(num)
